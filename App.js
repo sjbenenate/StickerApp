3a3a994ec6fd898f6b928594c87d74bc9b49c1f1
sjbenenate <sarahjoybenenate@gmail.com>
2024-05-27 15:35:47 -0700

    image selection WIP

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, ScrollView } from 'react-native';
-import { styles, toggleColorMode } from './styles_common';
+import { View, ScrollView } from 'react-native';
+import { styles } from './styles_common';
 import { ImageViewer } from './components/image-viewer';
 import { Button } from './components/button';
+import * as ImagePicker from 'expo-image-picker';
 
 const myImage = require('./assets/images/background-image.png');
 
 export default function App() {
+    const pickImageAsync = async () => {
+        const result = await ImagePicker.launchImageLibraryAsync({
+            allowsEditing: true,
+            quality: 1,
+        });
+
+        if (result.canceled) {
+            alert('Image not selected');
+            return;
+        } else {
+            console.log(result);
+            console.log(result.assets);
+        }
+    };
     return (
         <ScrollView>
             <View style={styles.container}>
                 <ImageViewer imageSource={myImage} />
                 <View style={styles.footer}>
-                    <Button title="Choose a photo" />
+                    <Button title="Choose a photo" onPress={pickImageAsync} />
                     <Button title="Use this photo" theme="secondary" />
                 </View>
 
